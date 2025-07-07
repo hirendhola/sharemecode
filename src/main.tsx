e2f6b50dbe5e66ccfca043b2b39a594c14faec64
hirendhola <hirendholap@gmail.com>
2025-07-08 02:48:51 +0530

    Analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </StrictMode>
